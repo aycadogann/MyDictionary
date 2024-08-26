939157c8fe35487c49a1fdb62fd39242d8687c04
Ayca Dogan <dogan.aycaa@gmail.com>
2024-08-26 14:42:17 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        K[] arrayKey;
+        V[] arrayValue;
+        K[] tempArrayKey;
+        V[] tempArrayValue;
+
+        public MyDictionary()
+        {
+            arrayKey = new K[0];
+            arrayValue = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            tempArrayKey = arrayKey;
+            tempArrayValue = arrayValue;
+            arrayKey = new K[arrayKey.Length + 1];
+            arrayValue = new V[arrayKey.Length + 1];
+
+
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                arrayKey[i] = tempArrayKey[i];
+                arrayValue[i] = tempArrayValue[i];
+            }
+            arrayKey[arrayKey.Length - 1] = key;
+            arrayValue[arrayKey.Length - 1] = value;
+
+        }
+
+        public int Count
+        {
+            get { return arrayKey.Length; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sehirler = new MyDictionary<int, string>();
+
+            sehirler.Add(26, "Eskişehir");
+            sehirler.Add(34, "İstanbul");
+            sehirler.Add(48, "Muğla");
+            sehirler.Add(35, "İzmir");
+            sehirler.Add(01, "Adana"); 
+            sehirler.Add(59, "Tekirdağ");
+            Console.WriteLine(sehirler.Count);
+
+            MyDictionary<string, string> sozluk = new MyDictionary<string, string>();
+            sozluk.Add("door", "kapı");
+            sozluk.Add("bag", "çanta");
+            Console.WriteLine(sozluk.Count);
+        }
+    }
+}
